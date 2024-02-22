91d20cf3b61eded75c05f43048ce7f508a3c14ce
Dennis O'Brien <dennis.obrien@eqware.net>
2024-02-22 13:52:22 -0800

    created a few more constant variables and reworked servo moving to use TimerEvent.

--- bball_track_and_pass/bball_track_and_pass.ino
+++ bball_track_and_pass/bball_track_and_pass.ino
@@ -4,7 +4,8 @@
 
 HUSKYLENS huskylens;
 SoftwareSerial mySerial(10, 11); // RX, TX
-TimerEvent myTimer;
+TimerEvent myLedTimer;
+TimerEvent myServoTimer;
 
 const int servoPin = 9; // Servo signal pin
 const int ledPin = 13; // LED control pin
@@ -12,23 +13,27 @@ const int relayPin = 12; // Relay control pin
 const int detectionPauseDuration = 5000; // Pause duration in milliseconds
 
 int servoPosition = 90; // Initial servo position
+int targetPosition = 0;
 bool objectDetected = false; // Flag to track object detection
 
 int savedObjectID = 1; // Change this to the ID of the saved object you want to track
 
-bool setup_done = 0;  // Assume setup is not done
 bool hRequest = 0;  // Assume that serial comm is not okay
 bool hLearned = 1;  // Assume that learning has been done
 bool hAvailable = 1;  // Assume nothing is available
 bool ledOn = 0;
 unsigned long offWaitTime = 0;
 unsigned long now = 0;
+unsigned long ledOffTime = 1000;
+unsigned long ledOnTime = 1000;
+unsigned long servoDelayTime = 10;
 
 void printResult(HUSKYLENSResult result);
 void smoothMoveServo(int targetPosition);
 void moveServo(int angle);
 void sendCommandToHuskyLens(String command, String parameter);
-void ledShutoff();
+void ledOff();
+void adjustServo();
 
 void setup() {
   Serial.begin(115200);
@@ -48,18 +53,21 @@ void setup() {
   pinMode(relayPin, OUTPUT);
   digitalWrite(relayPin, LOW); // Ensure the relay is initially off
 
-  myTimer.set(1000, &ledShutoff);
-  myTimer.disable();
+  myLedTimer.set(ledOnTime, &ledOff);
+  myLedTimer.disable();
+
+  myServoTimer.set(servoDelayTime, &adjustServo);
+  myServoTimer.disable();
 
   // Configure HuskyLens to recognize a specific object
   sendCommandToHuskyLens("SET_RECOGNITION_MODE", "LEARNED_OBJECT_1");
-  setup_done = 1;
   Serial.println("Setup Done!");
 }
 
 void loop() {
-  myTimer.update();
-  if (!setup_done) return;
+  myLedTimer.update();
+  myServoTimer.update();
+
   if (!huskylens.request()) {
     if (hRequest) Serial.println(F("Fail to request data from HUSKYLENS, recheck the connection!"));
     hRequest = 0;
@@ -86,8 +94,8 @@ void loop() {
         int posX = result.xCenter; // Get X position of the object
 
         // Invert the mapping to align servo movement with object position
-        int targetPosition = map(posX, 0, 320, 180, 0);
-        //smoothMoveServo(targetPosition);
+        targetPosition = map(posX, 0, 320, 180, 0);
+        myServoTimer.enable();
 
         if (!ledOn && (offWaitTime < millis())) {
           digitalWrite(ledPin, HIGH); // Turn on the LED
@@ -95,8 +103,8 @@ void loop() {
           Serial.println("Both HIGH!");
           ledOn = 1;
           offWaitTime = 0;
-          myTimer.reset();
-          myTimer.enable();
+          myLedTimer.reset();
+          myLedTimer.enable();
         }
       }
     }
@@ -108,13 +116,14 @@ void printResult(HUSKYLENSResult result) {
   // Your printResult function remains the same
 }
 
-void smoothMoveServo(int targetPosition) {
+void adjustServo() {
   int increment = (targetPosition - servoPosition > 0) ? 1 : -1;
-  while (servoPosition != targetPosition) {
+  if (servoPosition != targetPosition) {
     servoPosition += increment;
     moveServo(servoPosition);
-    delay(10); // Adjust this delay for smoother movement
   }
+  else
+    myServoTimer.disable();
 }
 
 void moveServo(int angle) {
@@ -131,12 +140,12 @@ void sendCommandToHuskyLens(String command, String parameter) {
   delay(100);  // Give time for the HuskyLens to process the command
 }
 
-void ledShutoff()
+void ledOff()
 {
   digitalWrite(ledPin, LOW); // Turn off the LED
   digitalWrite(relayPin, LOW); // Turn off the relay to deactivate the external light
   ledOn = 0;
   Serial.println("Both LOW!");
-  myTimer.disable();
-  offWaitTime = millis() + 1000;
+  myLedTimer.disable();
+  offWaitTime = millis() + ledOffTime;
 }
